19ca2b77bd18c42816fb10637aba61159412d090
Jose Ibargüen Posada <joseibarguenp@gmail.com>
2024-06-30 13:07:17 -0500

    First Commit

---
+++ src/bases/01-let-const.js
@@ -0,0 +1,20 @@
+//variables y constantes
+
+const nombre = 'Jose';
+const apellido = 'Ibarguen';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado);
+
+
+
+//var no se debe usar
+if ( true) {
+    const nombre = 'Pedro';
+    console.log(nombre);
+}
+
+
+console.log( valorDado);
